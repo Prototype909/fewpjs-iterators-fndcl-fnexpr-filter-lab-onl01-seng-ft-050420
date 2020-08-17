338d983b47cc1dd9ed2b75f83d4b99481f3ed668
Steve Anderson <prototype909@msn.com>
2020-08-17 20:31:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const findMatching = (drivers, string) => drivers.filter( d => d.toLowerCase() === string.toLowerCase());
 
 const fuzzyMatch = (drivers, string) => drivers.filter( d => d.slice(0, string.length) === string);
+
+const matchName = (drivers, string) => drivers.filter( d => d.name === string);
